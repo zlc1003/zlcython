078d82a6beca9c041f1de948961a0d73d7148e47
zhang lucas <649536862@qq.com>
2022-01-25 08:06:21 -0800

    '1'

--- main.py
+++ main.py
@@ -97,60 +97,66 @@ def 图形化窗口位置(x,y):
 def 刷新窗口():
     window.update()
 '''
+# 安装
+# pip install opencc-python-reimplemented
+
+# t2s - 繁体转简体（Traditional Chinese to Simplified Chinese）
+# s2t - 简体转繁体（Simplified Chinese to Traditional Chinese）
+# mix2t - 混合转繁体（Mixed to Traditional Chinese）
+# mix2s - 混合转简体（Mixed to Simplified Chinese）
 import opencc
 cc = opencc.OpenCC('t2s')
 import sys
-def tran_code_f(codefile,encodefile):
-    codefile=sys.argv[1]
-    encodefile=sys.argv[2] 
-    decodecode=''
-    useurllib_request=False
-    userandom=False
-    usetime=False
-    code=[]
-    with open(codefile,encoding="utf-8") as f:
-        _code=f.readlines()
-        for i in _code:
-            code.append(cc.convert(i))
-    for line in code:
-        lines=line.split()
-        tab=''
-        while True:
-            if line.startswith("    "):
-                tab+="    "
-                line=line[4:]
-                _=line==line[:]
-            else:
-                break
-        if len(lines)==0:
+codefile=sys.argv[1]
+encodefile=sys.argv[2] 
+decodecode=''
+useurllib_request=False
+userandom=False
+usetime=False
+code=[]
+with open(codefile,encoding="utf-8") as f:
+    _code=f.readlines()
+    for i in _code:
+        code.append(cc.convert(i))
+for line in code:
+    lines=line.split()
+    tab=''
+    while True:
+        if line.startswith("    "):
+            tab+="    "
+            line=line[4:]
+            _=line==line[:]
+        else:
+            break
+    if len(lines)==0:
+        continue
+    if len(lines)==1 and lines[0]=="导入图形化界面":
+        usegui=True
+        for guicodeline in guicode.split("\n"):
+            decodecode+=(tab+guicodeline+"\n")
+        continue
+    for i in range(len(lines)):
+        if "请求网页" in lines[i]:
+            useurllib_request=True
+        if "随机数" in lines[i]:
+            userandom=True
+        if "等待" in lines[i]:
+            usetime=True
+        if lines[i] in trandic:
+            lines[i]=trandic[lines[i]]
             continue
-        if len(lines)==1 and lines[0]=="导入图形化界面":
-            usegui=True
-            for guicodeline in guicode.split("\n"):
-                decodecode+=(tab+guicodeline+"\n")
-            continue
-        for i in range(len(lines)):
-            if "请求网页" in lines[i]:
-                useurllib_request=True
-            if "随机数" in lines[i]:
-                userandom=True
-            if "等待" in lines[i]:
-                usetime=True
-            if lines[i] in trandic:
-                lines[i]=trandic[lines[i]]
-                continue
-        replace_line=(' '.join(lines)+'\n')
-        for key,value in replace_trandic.items():
-            replace_line=replace_line.replace(key,value)
-        decodecode+=tab+replace_line
-    with open(encodefile,mode="w",encoding="utf-8") as f:
-        f.write("# -*- coding: utf-8 -*-\n")
-        f.write("#!python3"+"\n"*3)
-        if useurllib_request:
-            f.write("import urllib.request as get__"+"\n")
-        if userandom:
-            f.write("import random"+"\n")
-        if usetime:
-            f.write("import time"+"\n")
-        f.write(decodecode)
-        f.write("\n"*2)
+    replace_line=(' '.join(lines)+'\n')
+    for key,value in replace_trandic.items():
+        replace_line=replace_line.replace(key,value)
+    decodecode+=tab+replace_line
+with open(encodefile,mode="w",encoding="utf-8") as f:
+    f.write("# -*- coding: utf-8 -*-\n")
+    f.write("#!python3"+"\n"*3)
+    if useurllib_request:
+        f.write("import urllib.request as get__"+"\n")
+    if userandom:
+        f.write("import random"+"\n")
+    if usetime:
+        f.write("import time"+"\n")
+    f.write(decodecode)
+    f.write("\n"*2)
